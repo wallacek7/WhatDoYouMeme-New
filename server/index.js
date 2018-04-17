7fadea8b0d07fd46aeac8cd8decc6421c1a2ad94
Kayla <kaylaawallace7@gmail.com>
2018-04-17 15:50:44 -0400

    Middleware

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
-var express = require('express')
+const express = require('express');
+const path = require('path');
+
 var app = express()
 
 const servername = "localhost";
@@ -14,8 +16,12 @@ app
         res.header("Access-Control-Allow-Headers", "*");
         next();      
     })
+    .use('/', express.static(path.join(__dirname, "../dist/")))
     .use('/simple', simple)
     .use('/game', game)
+    .use('/', (req, res, next) => {
+        res.sendFile(path.join(__dirname, "../dist/index.html"));
+    })
     .listen(port);
 
 console.log("running on http://" + servername + ":" + port)
--- src/app/models/game.ts
+++ src/app/models/game.ts
@@ -7,7 +7,9 @@ export class Game {
         { Name: 'Donald Trump', MyQuotes: [] },
     ];
     Dealer: string = "Kayla Wallace";
-    Picture: string = 'https://media3.s-nbcnews.com/j/msnbc/components/video/201803/tdy_news_welker_trump_180315_1920x1080.nbcnews-ux-1080-600.jpg';
+    Picture: {
+        url: string
+    };
     PlayedQuotes: Quote[] = [
         { Text: "That's fake news", PlayerName: 'Donald Trump', Chosen: false }
     ];
