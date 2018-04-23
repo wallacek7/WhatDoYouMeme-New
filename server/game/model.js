2098c4ed89aed2ec1de5235a70622398b84baf6d
Kayla <kaylaawallace7@gmail.com>
2018-04-23 15:36:28 -0400

    Clear quotes for new round

--- server/game/model.js
+++ server/game/model.js
@@ -136,8 +136,11 @@ function Game() {
                 return QuotesStack.slice(iCurrentQuote, iCurrentQuote += 7);
         }
         
-        this.FlipPicture = () => this.Picture = PicturesStack[iCurrentPicture = (iCurrentPicture+1) % PicturesStack.length ];
-
+        this.FlipPicture = () => {
+            this.Picture = PicturesStack[iCurrentPicture = (iCurrentPicture+1) % PicturesStack.length ];
+            this.PlayedQuotes = [];
+        } 
+        
         this.SubmitQuote = (text, playerId) => {
             if(playerId == this.DealerId) throw Error("Dealer can't submit a quote");
             this.PlayedQuotes.push({ Text: text, PlayerId: playerId });
