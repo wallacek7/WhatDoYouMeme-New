d87885067c8581e8ec9ee3cf15b8c65af8d0c027
Kayla <kaylaawallace7@gmail.com>
2018-05-14 06:37:30 -0400

    Google Login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,27 +11,30 @@ declare var googleyolo: any;
 export class LoginComponent implements OnInit {
 
   constructor(private _Game: GameService) { 
-    googleyolo.hint({
-      supportedAuthMethods: [
-        "https://accounts.google.com",
-        //"googleyolo://id-and-password"
-      ],
-      supportedIdTokenProviders: [
-        {
-          uri: "https://accounts.google.com",
-          clientId: "815059487103-snnhjbdeq4pidmsgcnd2v39gsb7ks4sh.apps.googleusercontent.com"
-        }
-      ]
-    })
-    .then((credentials: any) => {
-      _Game.oAuthLogin(credentials.displayName, credentials.idToken, credentials.profilePicture);
-      console.log(credentials);
-    })
+    
   } 
   ngOnInit() {
   }
 
   login(name: string, password: string){
     this._Game.login(name, password);
+  }
+  googlelogin(){
+  googleyolo.hint({
+    supportedAuthMethods: [
+      "https://accounts.google.com",
+      //"googleyolo://id-and-password"
+    ],
+    supportedIdTokenProviders: [
+      {
+        uri: "https://accounts.google.com",
+        clientId: "815059487103-snnhjbdeq4pidmsgcnd2v39gsb7ks4sh.apps.googleusercontent.com"
+      }
+    ]
+  })
+    .then((credentials: any) => {
+      this._Game.oAuthLogin(credentials.displayName, credentials.idToken, credentials.profilePicture);
+      console.log(credentials);
+    })
   }
 }
